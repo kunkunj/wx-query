c3b617a6ca39ec304cf135c4404f84f00c9fa3a0
lukun <lukun@inoview.cn>
2023-05-26 11:13:52 +0800

    lk

--- package/pages/t/t.js
+++ package/pages/t/t.js
@@ -1,8 +1,11 @@
 import { store } from '../../store/index'
-const { wxPage } = require('../../sdk/index.js')
-
+const { wxPage,WxRequest } = require('../../sdk/index.js')
+wxPage.use(WxRequest)
 wxPage.init({
     Store:[store],
+    onLoad(){
+      this.$.$request({url:'https://www.isqqw.com/viewer?id=38151'})
+    },
     changeStore(){
         store.changeName()
     }
